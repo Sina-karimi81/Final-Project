a07840beb2a07b92f6388bdf8573f002e3149530
amirsallah <amir1381salah@aut.ac.ir>
2021-07-15 01:24:57 +0430

    fix: completed SignUp.fxml and SignUpController

--- src/Controller/SignUpController.java
+++ src/Controller/SignUpController.java
@@ -6,13 +6,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class SignUpController {
 
@@ -21,6 +20,9 @@ public class SignUpController {
 
     @FXML
     private PasswordField SignPass;
+
+    @FXML
+    private Label error;
 
     @FXML
     void Login() throws IOException {
@@ -33,16 +35,32 @@ public class SignUpController {
 
     @FXML
     void SignUp() throws IOException {
-        String username = SignText.getText();
-        String password = SignPass.getText();
-        User user = new User(username,password,0,1);
-        ShareData.setPlayer(user);
-        saveToFile(user);
-        Stage primaryStage = (Stage) SignPass.getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getResource("../View/MainMenu.fxml"));
-        primaryStage.setTitle("MainMenu");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+            String username = SignText.getText();
+            String password = SignPass.getText();
+            if (password.length() >= 8) {
+
+                try (FileInputStream fileInputStream = new FileInputStream("Save.txt")){
+                    while (fileInputStream.available() != 0){
+                        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+                        User user = (User) objectInputStream.readObject();
+                        if (user.getUserName().equals(username)){
+                            error.setText("This name already exists");
+                            return;
+                        }
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                User user = new User(username, password, 0, 1);
+                ShareData.setPlayer(user);
+                saveToFile(user);
+                Stage primaryStage = (Stage) SignPass.getScene().getWindow();
+                Parent root = FXMLLoader.load(getClass().getResource("../View/MainMenu.fxml"));
+                primaryStage.setTitle("MainMenu");
+                primaryStage.setScene(new Scene(root));
+                primaryStage.show();
+            }else
+                error.setText("password must be more than 8 characters");
     }
 
     void saveToFile(User user){
