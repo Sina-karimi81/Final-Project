463f254e9c39f74e0b605f2a835733ec36e5b46e
amirsallah <amir1381salah@aut.ac.ir>
2021-07-20 02:35:57 +0430

    add: add Image and URL field for Card class and add constructor for it. amir sallah

--- src/Cards/Card.java
+++ src/Cards/Card.java
@@ -1,4 +1,32 @@
 package Cards;
 
-public class Card {
+import javafx.scene.image.Image;
+
+import java.io.Serializable;
+
+public class Card implements Serializable {
+    private final String image;
+    private transient Image imageView;
+
+    public Card(String image) {
+        this.image = image;
+        imageView = new Image(String.valueOf(getClass().getResource(image)));
+    }
+
+    public Image getImageView() {
+        return imageView;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImageCard(){
+        imageView = new Image(String.valueOf(getClass().getResource(image)));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return this.image.equals(((Card) o).getImage());
+    }
 }
